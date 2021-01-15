5d552a6bc0cc53b8f373a6b4a59a2c59a3f0e812
PenLoo98 <sohnchanho@gmail.com>
2021-01-15 18:06:22 +0900

    Pyqt5 Practice

--- Lab.py
+++ Lab.py
@@ -1,40 +1,33 @@
-global count
-count = 0
-
-while count == 0:  # 반복입력 피하기 (처음 입력시 0)
-    try:  # 설정시간 입력 + 오류 보완
-        start_time = int(input("시작 시간을 입력하시오 \n예)24시15분 → 0015\n:"))
-        end_time = int(input("끝 시간을 입력하시오 \n예)18시50분 → 1850\n:"))
-
-    except ValueError:  # 오류 - 글자나 공백을 입력할 경우
-        print("글자 또는 공백은 입력할 수 없습니다")
-        print('==================================================')
-        continue
-
-            #  입력 오류 보완
-    if start_time // 10000 or end_time // 10000:  # 자릿수 4자리 초과
-        print("잘못된 입력입니다")
-        print('==================================================')
-    else:
-        count += 1  # 제대로 된 값이 입력되어 카운트 1
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
+from PyQt5.QtGui import QIcon
 
 
-def time_select(s_hour_minute, e_hour_minute):
-    import time  # 현재시간 파악
-    T = time.strftime('%X', time.localtime(time.time()))
-    hour_minute_second = T.split(":")
-    hour_minute = int(hour_minute_second[0]+hour_minute_second[1])
+class MyApp(QMainWindow):
 
-        import ctypes
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:/14.jpg", 0)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        exitAction = QAction(QIcon('C:\\Users\\sohnc\\Desktop\\exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
+
+        self.setWindowTitle('Menubar')
+        self.setGeometry(300, 300, 300, 300)
+        self.show()
 
 
-time_select(start_hour, start_minute, end_hour, end_minute)
-
-    '''
-        def Picture_save:
-        
-        def Change_Background:
-            import ctypes
-            ctypes.windll.user32.SystemParametersInfoW(20, 0,"C:/14.jpg" , 0)
-    '''
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
--- Time-Based Background Changer ver.3.py
+++ Time-Based Background Changer ver.3.py
@@ -10,16 +10,13 @@ def select_time(start_time, end_time, address):
         hour_minute_second = current_time.split(":")
         real_time = int(hour_minute_second[0] + hour_minute_second[1])
 
-        if (int(hour_minute_second[1]) == 0) or (int(hour_minute_second[1]) == 30):  # 정각
-            if start_time <= real_time <= end_time:
-                import ctypes
-                ctypes.windll.user32.SystemParametersInfoW(20, 0, address, 0)
+        if start_time <= real_time <= end_time:
+            import ctypes
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, address, 0)
+        time.sleep(60)
 
 
-select_time(1700, 1800, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/ 12.jpg")
-select_time(1800, 1900, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/ 13.jpg")
-select_time(1900, 2000, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/ 14.jpg")
-# 1) 클래스를 이용해서 제대로 된 인스턴스를 만들 수 있는가? >> 클래스가 꼭 필요해보이진 않는다
-# 2) 23시~1시 사이에 시간 문제를 해결 했는가? >> 시작 시간과 끝 시간 사이에 자정(0시)시간이 올 수 없도록 설정
-# 3) 모르는 내용, 해결하고 싶은 문제를 제대로 파악했는가?
-# 4) 시간체크 주기를 얼마나 할지 정했는가 >> 1시간
+#  사진 디렉토리 띄어쓰기 주의!
+select_time(1700, 1800, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/12.jpg")
+select_time(1800, 1900, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/13.jpg")
+select_time(1900, 2000, "C:/Users/sohnc/Desktop/잡/나를 위한 배경화면/1 생산활동 시간/14.jpg")
