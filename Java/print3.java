f6c8c2bae208124a5207c8dd20d7f49ed839d5b7
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-01-21 18:14:15 +0900

    Update print3.java

--- Java/print3.java
+++ Java/print3.java
@@ -1,7 +1,13 @@
 public class print3 {
     public static void main (String[] args) {
-        System.out.print("Hello World!");
-        System.out.println("Hello World!");
-        System.out.printf("Hello World!");
+        int a = 3;
+        System.out.print(a);
+        System.out.print(a+"\n");
+        System.out.println(a);
+        System.out.printf("a is %d", a);
     }
 }
+
+// 실행결과 
+// 33
+// a is 3
