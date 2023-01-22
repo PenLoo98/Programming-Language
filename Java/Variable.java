c6a6ba617efe6c2cb3bebd5ffdafd2bdcd7fc6c3
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2023-01-22 19:53:46 +0900

    make Variable.java

---
+++ Java/Variable.java
@@ -0,0 +1,12 @@
+public class Variable{
+    public static void main(String[] args){
+        int count=1;
+        System.out.println("count == "+count);
+        count++;
+        System.out.println("count+1 == "+count);
+        
+        double stack=1.5;
+        System.out.println("stack is "+stack);
+        stack+=1.7;
+        System.out.println("stack+1.7 is "+stack);
+    }};
